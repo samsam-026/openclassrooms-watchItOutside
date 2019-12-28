e1125fdd95ea582cef9dcfe8b3ec15d81c7c7ee1
Sameeha Rahman <rahman.sameeha@gmail.com>
2019-12-28 16:21:10 +0530

    Update countdown code

--- assets/js/countdown.js
+++ assets/js/countdown.js
@@ -10,19 +10,30 @@ var x = setInterval(function () {
     // Find the distance between now and the count down date
     var distance = countDownDate - now;
 
-    // Time calculations for days, hours, minutes and seconds
-    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-
-    // Display the result in the element with id="countdown"
-    document.getElementById("countdown").innerHTML = days + "d " + hours + "h "
-        + minutes + "m " + seconds + "s ";
-
     // If the count down is finished, write some text
     if (distance < 0) {
+        
         clearInterval(x);
         document.getElementById("countdown").innerHTML = "Watch It Ouside Boston has begun!";
+
+    } else {
+
+        // Time calculations for days, hours, minutes and seconds
+        var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+        var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+        var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+        var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+        // Display the result in the element with id="countdown"
+        if (days > 0) {
+            document.getElementById("countdown").innerHTML = days + "d " + hours + "h " + minutes + "m " + seconds + "s ";
+        } else if (hours > 0) {
+            document.getElementById("countdown").innerHTML = hours + "h " + minutes + "m " + seconds + "s ";
+        } else if (minutes > 0) {
+            document.getElementById("countdown").innerHTML = minutes + "m " + seconds + "s ";
+        } else {
+            document.getElementById("countdown").innerHTML = seconds + "s ";
+        }
     }
+
 }, 1000);
